887624662e50c0d8bbc7d9ebf947ee8b8c5be038
K250095MohsinaZeeshan <K250095@nu.edu.pk>
2025-12-04 18:55:24 +0500

    Add files via upload

---
+++ Task01_LAB12.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("ONLINE BOOKSTORE TRACKER\n");
+	printf("________________________\n");
+    int n, i, updateStock;
+    int *stock;
+    int total = 0, minStock = 0;
+
+    printf("\nEnter number of book categories: ");
+    scanf("%d", &n);
+
+    stock = (int *)malloc(n * sizeof(int));
+    if (!stock) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+    for (i = 0; i < n; i++) {
+        printf("Enter stock for category %d: ", i);
+        scanf("%d", &stock[i]);
+        total += stock[i];
+        if (stock[i] < stock[minStock])
+            minStock = i;
+    }
+    printf("\nTotal Stock = %d\n", total);
+    printf("Average Stock per category = %.2f\n", (float)total / n);
+    printf("Category with lowest stock = %d\n", minStock);
+
+    printf("\nEnter category index to update: ");
+    scanf("%d", &updateStock);
+    if (updateStock >= 0 && updateStock < n) {
+        printf("Enter new stock value: ");
+        scanf("%d", &stock[updateStock]);
+    } else {
+        printf("Invalid index!\n");
+    }
+    free(stock);
+    return 0;
+}
+
